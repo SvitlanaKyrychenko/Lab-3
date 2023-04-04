0de9b9e41fc7b0cccd06e133805352ae705e5e56
SvitlanaKyrychenko <svitlana.kyrychenko@ukma.edu.ua>
2023-04-04 21:12:10 +0200

    fix

---
+++ superapp/server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express')
+const path = require('path')
+
+const PORT = 3000
+
+const app = express()
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Running on port:${PORT}`)
+})
